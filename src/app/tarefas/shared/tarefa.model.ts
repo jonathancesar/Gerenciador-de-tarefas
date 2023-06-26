0ebd67bb0725c6a0fc7ddbaddf3e31bf02f1ccc5
jonathancesar <jonathancesar1995@gmail.com>
2023-06-26 20:23:04 -0300

    Primeiro commit

---
+++ src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,8 @@
+export class Tarefa{
+
+    constructor(
+        public id?: number,
+        public nome?: string,
+        public concluida?: boolean){}
+    // ? significa que seja opcional, criar valores sem se for obrigatório
+}
